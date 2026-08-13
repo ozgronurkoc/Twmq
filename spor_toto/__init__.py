17d34a7369c9cf38dd828eaa14bf20ec5b8e01eb
postovral <44780550-postovral@users.noreply.replit.com>
2026-08-13 12:28:50 +0000

    Use importlib.metadata to expose __version__ in spor_toto/__init__.py
    Replit-Task-Id: d4b2f208-8316-4fe2-990f-dc7c71f2caa3

--- spor_toto/__init__.py
+++ spor_toto/__init__.py
@@ -1,6 +1,6 @@
-from importlib.metadata import version, PackageNotFoundError
-
 try:
+    from importlib.metadata import version, PackageNotFoundError
+
     __version__ = version("spor-toto-kapsama")
 except PackageNotFoundError:
-    __version__ = "0.0.0"
+    __version__ = "4.0.0"
